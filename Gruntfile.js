a3af777467e0b9f2b80c6bf4b84aff49e67db299
Phil <birkal@outlook.com>
2015-01-17 15:59:27 -0800

    Setup site layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,6 @@ module.exports = function(grunt) {
       }
     },
 
-    stylus: {
-      compile: {
-        files: {
-          'css/custom.css': 'stylus/main.styl'
-        }
-      }
-    },
-
     concat: {
       css: {
         files: {
@@ -38,28 +30,19 @@ module.exports = function(grunt) {
       jade: {
         files: 'src/*.jade',
         tasks: ['jade']
-      },
-      stylus: {
-        files: 'stylus/*.styl',
-        tasks: ['stylus']
-      },
-      minify: {
-        files: 'css/*.css',
-        tasks: ['concat', 'cssmin']
       }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('html', ['jade']);
-  grunt.registerTask('css', ['stylus', 'concat', 'cssmin']);
+  grunt.registerTask('css', ['concat', 'cssmin']);
 
-  grunt.registerTask('default', ['jade', 'stylus', 'concat', 'cssmin', 'watch']);
+  grunt.registerTask('default', ['jade', 'concat', 'cssmin', 'watch']);
 
 };
