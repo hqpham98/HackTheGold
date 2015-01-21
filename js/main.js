708071b564cca5c2b4391a4ef7125eb6b546103d
Aaron Dancer <me@aarondancer.com>
2015-01-21 13:23:31 -0600

    Improved vcentering

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function positionNav() {
-    $('.bannerText').css("top", ($("#home").height() - $("#HackTheGold").height()-120)/2);
+    $('.bannerText').css("top", ($("#home").height() - $("#HackTheGold").height()-140)/2);
 }
 
 $(window).resize(function() {
