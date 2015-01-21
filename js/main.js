82edf02fe9bb3d9720c19caa3cfef8e790e2d982
Aaron Dancer <me@aarondancer.com>
2015-01-21 12:52:36 -0600

    CSS Vertical centering home.
    Needs to be tested on mobile

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function positionNav() {
-    $('.bannerText').css("top", ($("#home").height() - $("#HackTheGold").height()-60)/2);
-}
+// function positionNav() {
+//     $('.bannerText').css("top", ($("#home").height() - $("#HackTheGold").height()-60)/2);
+// }
 
-$(window).resize(function() {
-    positionNav();
-});
+// $(window).resize(function() {
+//     positionNav();
+// });
 
-jQuery(window).load(function () {
-    positionNav();
-});
+// jQuery(window).load(function () {
+//     positionNav();
+// });
