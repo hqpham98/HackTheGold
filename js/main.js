71652c5e8990d0b06e123ea9ddf494b93c746d66
Aaron Dancer <me@aarondancer.com>
2015-01-21 12:50:13 -0600

    Wait for page to fully load

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-$(function() {
-    function positionNav() {
-        $('.bannerText').css("top", ($("#home").height() - $("#HackTheGold").height()-60)/2);
-    }
-    
-    $(window).resize(function() {
-        positionNav();
-    });
+function positionNav() {
+    $('.bannerText').css("top", ($("#home").height() - $("#HackTheGold").height()-60)/2);
+}
 
+$(window).resize(function() {
     positionNav();
 });
 
+jQuery(window).load(function () {
+    positionNav();
+});
